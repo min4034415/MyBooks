d52eed4863f221f137afb18d5ca3a63f47c7c460
min4034415 <min4034415@gmail.com>
2024-09-25 22:57:48 +0900

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Ouimin Lee on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
