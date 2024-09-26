9ae2c9929d61e47d3d0730f19fcb09c0135edd01
min4034415 <min4034415@gmail.com>
2024-09-26 11:20:59 +0900

    Femininomenon

--- MyBooks/Book.swift
+++ MyBooks/Book.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var dateAdded: Date
+    var dateCompleted: Date
+    var summary: String
+    var rating: Int?
+    var status: Status
+    
+    init(
+        title: String,
+        author: String,
+        dateAdded: Date = Date.now,
+        dateCompleted: Date = Date.distantPast,
+        summary: String = ""  ,
+        rating: Int? = nil,
+        status: Status = .onShelf
+    ) {
+        self.title = title
+        self.author = author
+        self.dateAdded = dateAdded
+        self.dateCompleted = dateCompleted
+        self.summary = summary
+        self.rating = rating
+        self.status = status
+    }
+}
+
+
+enum Status: Int, Codable, Identifiable, CaseIterable {
+    case onShelf, inProgress, completed
+    var id: Self {
+        self
+    }
+    var descr: String {
+        switch self {
+        case .onShelf:
+            "On Shelf"
+        case .inProgress:
+            "In Progress"
+        case .completed:
+            "Completed"
+        }
+    }
+}
