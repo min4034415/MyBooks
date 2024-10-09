7ea5e4f5939d41fb10806f00d596de4b12515c34
min4034415 <min4034415@gmail.com>
2024-10-09 16:08:17 +0900

    다비치 사랑사랑아

---
+++ MyBooks/Quote.swift
@@ -0,0 +1,23 @@
+//
+//  Quote.swift
+//  MyBooks
+//
+//  Created by Ouimin Lee on 10/9/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Quote {
+    var creationDate: Date = Date.now
+    var text: String
+    var page: String?
+    
+    init(text: String, page: String? = nil) {
+        self.text = text
+        self.page = page
+    }
+    
+    var book: Book?
+}
---
+++ MyBooks/QuotesListView.swift
@@ -0,0 +1,20 @@
+//
+//  QuotesListView.swift
+//  MyBooks
+//
+//  Created by Ouimin Lee on 10/9/24.
+//
+
+import SwiftUI
+
+struct QuotesListView: View {
+    @Environment(\.modelContext) private var modelContext
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuotesListView()
+}
